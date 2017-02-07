e1d41ff35f80ae193bf669d14f58353a1539957f
KitFung <sa9510@gmail.com>
2017-02-07 20:03:34 +0800

    tmp

--- task2/GENIE3_python/upstream_regulator.py
+++ task2/GENIE3_python/upstream_regulator.py
@@ -98,6 +98,15 @@ predicted_state = cal_predicted_state(VIM, direction, observed_regulation)
 # - top regulator regulated gene
 
 # use > 0.01 as a threshold to filter out the weak relationship
+def form_TR2TGs(TRs, VIM):
+    TR2TGs = {}
+    for TR in TRs:
+        TGs_case = VIM.loc[TR] > 0
+        TGs = TGs_case[TGs_case].index
+        TR2TGs[TR] = TGs
+    return TR2TGs
+
+
 def cal_overlap_pvalue(all_genes, regulated_gene):
     all_genes = set(all_genes)
     regulated_gene = set(regulated_gene)
@@ -107,23 +116,65 @@ def cal_overlap_pvalue(all_genes, regulated_gene):
     return pvalue
 
 
-def filter_TR_by_pvalue(VIM, dataset_genes):
+def filter_TR_by_pvalue(TRs, TR2TGs, VIM, dataset_genes):
     significant_TRs = []
-    for TR in VIM.index:
-        TG_case = VIM.loc[TR] > 0
-        TG = TG_case[TG_case].index
-        pvalue = cal_overlap_pvalue(dataset_genes, TG)
+    for TR in TRs:
+        TGs = TR2TGs[TR]
+        pvalue = cal_overlap_pvalue(dataset_genes, TGs)
         if pvalue < 0.01:
             significant_TRs.append(TR)
     return significant_TRs
 
 
+TRs = VIM.index
 sub_sampling_genes = gene_names[::2]
-TRs_after_pvalue = filter_TR_by_pvalue(VIM, sub_sampling_genes)
+TR2TGs = form_TR2TGs(TRs, VIM)
+TRs_after_pvalue = filter_TR_by_pvalue(TRs, TR2TGs, VIM, sub_sampling_genes)
 
 
 # z-score
-def cal_zscore(VIM, TRs, weight, predicted_state):
+def cal_zscore(TRs, TR2TGs, weight, predicted_state):
+    z_scores = {}
     for TR in TRs:
-        pass
-    pass
+        z_scores[TR] = 0
+        TGs = TR2TGs[TR]
+        for TG in TGs:
+            z_scores[TR] += weight.loc[TR, TG] * predicted_state.loc[TR, TG]
+    return z_scores
+
+
+z_scores = cal_zscore(TRs, TR2TGs, VIM, predicted_state)
+
+
+# bias = bias_data * bias_TR
+def cal_bias_data(observed_regulation):
+    N_up = observed_regulation[observed_regulation > 0].count()['cond']
+    N_down = observed_regulation[observed_regulation < 0].count()['cond']
+    return float(N_up - N_down)/(N_up + N_down)
+
+
+def cal_bias_TR(TR, TGs, direction):
+    tTR = direction.loc[TR, TGs]
+    activating = tTR[tTR > 0].count()
+    inhibiting = tTR[tTR < 0].count()
+    return float(activating - inhibiting)/(activating + inhibiting)
+
+
+def cal_bias(TRs, TR2TGs, direction, bias_data):
+    bias = {}
+    for TR in TRs:
+        bias[TR] = bias_data * cal_bias_TR(TR, TR2TGs[TR], direction)
+    return bias
+
+
+def filter_TR_by_bias(TRs, bias):
+    significant_TRs = []
+    for TR in TRs:
+        if bias[TR] < 0.25:
+            significant_TRs.append(TR)
+    return significant_TRs
+
+
+bias_data = cal_bias_data(observed_regulation)
+bias = cal_bias(TRs_after_pvalue, TR2TGs,  direction, bias_data)
+TRs_after_bias = filter_TR_by_bias(TRs_after_pvalue, bias)
